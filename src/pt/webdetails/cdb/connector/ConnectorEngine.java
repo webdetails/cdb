79ee633de8200aafe793d063ba2a6ac4a9950839
Pedro Pinheiro <pdpinheiro@gmail.com>
2012-10-04 11:32:26 +0100

    Refactor connectors and queries so into a single Query entity

--- src/pt/webdetails/cdb/connector/ConnectorEngine.java
+++ src/pt/webdetails/cdb/connector/ConnectorEngine.java
@@ -10,12 +10,14 @@ import java.util.Map;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.pentaho.platform.api.engine.IParameterProvider;
 import org.pentaho.platform.engine.core.system.PentahoSessionHolder;
 import pt.webdetails.cda.connections.Connection;
 import pt.webdetails.cda.dataaccess.DataAccess;
 import pt.webdetails.cda.settings.CdaSettings;
+import pt.webdetails.cdb.query.Query;
 import pt.webdetails.cpf.persistence.PersistenceEngine;
 import pt.webdetails.cpf.repository.RepositoryAccess;
 
@@ -49,8 +51,35 @@ public class ConnectorEngine {
     }
   }
 
+  protected Query getQuery(String key) throws ConnectorRuntimeException, ConnectorNotFoundException {
+    PersistenceEngine eng = PersistenceEngine.getInstance();
+    try {
+      Map<String, Object> params = new HashMap<String, Object>();
+      params.put("id", key);
+      JSONObject response = eng.query("select * from Query where @rid = :id", params);
+      JSONObject query = (JSONObject) ((JSONArray) response.get("object")).get(0);
+      return getQuery(query);
+    } catch (JSONException e) {
+      throw new ConnectorRuntimeException(e);
+    }
+  }
+
+  protected Query getQuery(JSONObject json) throws ConnectorRuntimeException, ConnectorNotFoundException {
+    try {
+      String type = json.getString("type");
+      Class<?> queryClass = Class.forName("pt.webdetails.cdb.query." + type + "Query");
+      Query q = (Query) queryClass.getConstructor().newInstance();
+      q.fromJSON(json);
+      return q;
+    } catch (ClassNotFoundException e) {
+      throw new ConnectorNotFoundException(e);
+    } catch (Exception e) {
+      throw new ConnectorRuntimeException(e);
+    }
+  }
+
   public String[] listConnectors() {
-    String[] types = {"Saiku"};
+    String[] types = {"Saiku", "SQL"};
     return types;
   }
 
@@ -58,31 +87,32 @@ public class ConnectorEngine {
     PersistenceEngine eng = PersistenceEngine.getInstance();
     try {
 
-      Map<String,Object> params = new HashMap<String,Object>();
-      params.put("group",groupId);
-      params.put("user",PentahoSessionHolder.getSession().getName());
-      
-      
+      Map<String, Object> params = new HashMap<String, Object>();
+      params.put("group", groupId);
+      params.put("user", PentahoSessionHolder.getSession().getName());
+
+
       // DISABLING MULTI USER SUPPORT BY NOW JSONObject response = eng.query("select * from Query where group = :group and userid = :user",params);
-      JSONObject response = eng.query("select * from Query where group = :group ",params);
-      
-      
+      JSONObject response = eng.query("select * from Query where group = :group ", params);
+
+
       JSONArray queries = (JSONArray) response.get("object");
       CdaSettings cda = new CdaSettings(groupId, null);
       for (int i = 0; i < queries.length(); i++) {
         JSONObject query = queries.getJSONObject(i);
         String type = query.getString("type");
+        Query q = ConnectorEngine.getInstance().getQuery(query);
         try {
           Connector conn = ConnectorEngine.getInstance().getConnector(type);
 
-          Connection connection = conn.exportCdaConnection(query);
+          Connection connection = q.exportCdaConnection();
           if (connection == null) {
             logger.error("Connection is null. Something is wrong with query "
                     + query.getString("name") + ". Ignoring.");
             continue;
           }
           cda.addConnection(connection);
-          DataAccess dataAccess = conn.exportCdaDataAccess(query);
+          DataAccess dataAccess = q.exportCdaDataAccess();
           cda.addDataAccess(dataAccess);
         } catch (ConnectorException e) {
           logger.error(e);
@@ -91,7 +121,7 @@ public class ConnectorEngine {
       String fileName = PentahoSessionHolder.getSession().getName() + "." + groupId + ".cda";
       RepositoryAccess repository = RepositoryAccess.getRepository();
       repository.publishFile("cdb/queries/" + fileName, cda.asXML(), true);
-      
+
       //RepositoryUtils.writeSolutionFile("cdb/queries", PentahoSessionHolder.getSession().getName() + "." + groupId + ".cda", cda.asXML());
     } catch (Exception e) {
       logger.error(e);
@@ -119,14 +149,7 @@ public class ConnectorEngine {
   public void copyQuery(String id, String newGuid) {
     PersistenceEngine eng = PersistenceEngine.getInstance();
     try {
-
-      Map<String,Object> params = new HashMap<String,Object>();
-      params.put("id",id);
-      JSONObject response = eng.query("select * from Query where @rid = :id",params);
-      JSONObject query = (JSONObject) ((JSONArray) response.get("object")).get(0);
-      String type = query.get("type").toString(),
-              oldGuid = query.get("guid").toString();
-      getConnector(type).copyQuery(oldGuid, newGuid);
+      getQuery(id).copy(newGuid);
     } catch (Exception e) {
       logger.error(e);
     }
@@ -135,13 +158,7 @@ public class ConnectorEngine {
   public void deleteQuery(String id) {
     PersistenceEngine eng = PersistenceEngine.getInstance();
     try {
-      Map<String,Object> params = new HashMap<String,Object>();
-      params.put("id",id);
-      JSONObject response = eng.query("select type from Query where @rid = :id",params);
-      JSONObject query = (JSONObject) ((JSONArray) response.get("object")).get(0);
-      String type = query.get("type").toString(),
-              guid = query.get("guid").toString();
-      getConnector(type).deleteQuery(guid);
+      getQuery(id).delete();
     } catch (Exception e) {
       logger.error(e);
     }
---
+++ src/pt/webdetails/cdb/query/SQLQuery.java
@@ -0,0 +1,70 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pt.webdetails.cdb.query;
+
+import java.io.IOException;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.json.JSONException;
+import org.json.JSONObject;
+import pt.webdetails.cda.connections.Connection;
+import pt.webdetails.cda.connections.sql.JndiConnection;
+import pt.webdetails.cda.connections.sql.SqlJndiConnectionInfo;
+import pt.webdetails.cda.dataaccess.DataAccess;
+import pt.webdetails.cda.dataaccess.SqlDataAccess;
+import pt.webdetails.cpf.Util;
+import pt.webdetails.cpf.repository.RepositoryAccess;
+
+/**
+ *
+ * @author pdpi
+ */
+public class SQLQuery extends AbstractQuery {
+
+  private static final Log logger = LogFactory.getLog(SQLQuery.class);
+  private static final String path = "cdb/saiku";
+
+  @Override
+  public DataAccess exportCdaDataAccess() {
+    String id, name, queryContent;
+    id = getName();
+    name = id;
+    queryContent = getProperty("query").toString();
+    DataAccess dataAccess = new SqlDataAccess(id, name, id, queryContent);
+    //dataAccess;
+    return dataAccess;
+  }
+
+  @Override
+  public Connection exportCdaConnection() {
+    String jndi;
+    jndi = getProperty("jndi").toString();
+    SqlJndiConnectionInfo cinfo = new SqlJndiConnectionInfo(jndi, null, null, null, null);
+    Connection conn = new JndiConnection(getName(), cinfo);
+    return conn;
+  }
+
+  @Override
+  public void copy(String newGuid) {
+    /* No-op */
+  }
+
+  @Override
+  public void delete() {
+    /* No-op */
+  }
+
+  @Override
+  public JSONObject toJSON() {
+    JSONObject json = super.toJSON();
+    return json;
+  }
+
+  @Override
+  public void fromJSON(JSONObject json) {
+    super.fromJSON(json);
+  }
+
+}
