d0c5934096f4675ca92b4ee662a856bff3879a2a
Pedro Vale <pedro.vale@webdetails.pt>
2012-12-11 00:34:39 +0000

    merge

--- src/pt/webdetails/cdb/exporters/DynamicPythonExporter.java
+++ src/pt/webdetails/cdb/exporters/DynamicPythonExporter.java
@@ -17,30 +17,7 @@ public class DynamicPythonExporter extends AbstractExporter {
 
   public DynamicPythonExporter() {
     this.fileExportExtension = "py";
-  }
-    
-  static final private String LOAD_CDB = "def readCdb(url, group, id):\n"
-          + "  import csv, getpass, urllib\n"
-          + "  # We need to read in the user and the password for the Pentaho\n"
-          + "  # server, so we make sure to escape all the bits of the URL we'e building\n"
-          + "  user = \"&userid=\" + urllib.quote(raw_input(\"User: \"))\n"
-          + "  password = \"&password=\" + urllib.quote(getpass.getpass())\n"
-          + "  base_path = \"/content/cdb/doQuery?outputType=csv\"\n"
-          + "  file = \"&group=\" + urllib.quote(group)\n"
-          + "  data_id = \"&id=\" + urllib.quote(id)\n"
-          + "  complete_url = url + base_path + file + data_id + user + password\n"
-          + "  # Open the CSV file, detect the format (commas or semicolons? etc),\n"
-          + "  # rewind back to the start of the file, and parse it\n"
-          + "  csv_file = open(urllib.urlretrieve(complete_url)[0])\n"
-          + "  dialect = csv.Sniffer().sniff(csv_file.read(1024))\n"
-          + "  csv_file.seek(0)\n"
-          + "  return csv.reader(csv_file, dialect)\n"
-          + "\n";
-
-  @Override
-  public String export(String group, String id, String url) {
-    String loadString = "cdbData = readCdb(\"" + url + "\", \"" + group + "\", \"" + id + "\");\n";
-    return LOAD_CDB + loadString;
+    this.templateFile = "DynamicPython.mustache";
   }
 
 }
